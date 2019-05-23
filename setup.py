4e634e403d326387c22d10858714ad888419f9f5
Philip Fowler <philipwfowler@gmail.com>
2019-05-23 20:41:48 +0100

    renamed gasta -> vasta

--- setup.py
+++ setup.py
@@ -1,20 +1,21 @@
 from setuptools import setup
-from piezo import __version__
+from vasta import __version__
 
 setup(
-    name='gasta',
+    name='vasta',
     version=__version__,
     author='Philip W Fowler',
     author_email="philip.fowler@ndm.ox.ac.uk",
-    packages=['gasta'],
+    packages=['vasta'],
     package_data={'': ['../config/*']},
     install_requires=[
         "numpy >= 1.13",
         "PyVCF >= 0.6.8",
         "Biopython >= 1.70",
+        "h5py >= 2.8.0"
     ],
     license='MIT',
-    scripts=['bin/gasta-run.py'],
+    scripts=['bin/vasta-run.py'],
     long_description=open('README.md').read(),
     zip_safe=False
 )
